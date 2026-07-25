d9f4c3d6b5e236e55db2c4e679d57199dd3d8ef5
Prathmesh Sharma <prathmesh.sharma101@gmail.com>
2026-07-25 19:21:09 +0530

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+void recurse(TreeNode* root, unordered_map<TreeNode*, int>& maxmap, int x) {
+    if (!root) return;
+    maxmap[root] = max(x, root->val);
+    recurse(root->left, maxmap, maxmap[root]);
+    recurse(root->right, maxmap, maxmap[root]);
+}
+int postorder(TreeNode* root, unordered_map<TreeNode*, int>& maxmap) {
+    if (!root) return 0;
+        int left = postorder(root->left, maxmap);
+        int right = postorder(root->right, maxmap);
+    if (root->val >= maxmap[root]) {
+        return left+right+1;
+    } else {
+        return left+right;
+    }
+    return 0;
+}
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        unordered_map<TreeNode*, int> maxmap;
+        recurse(root, maxmap, root->val);
+        return postorder(root, maxmap);
+    }
+};
